0e8449cf2c2a851b3ef128928e19449266417dfb
RachelMinto <rachel.j.minto@gmail.com>
2016-07-09 15:51:39 -0400

    Refactored record_branch.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -4,13 +4,13 @@ class Bst
   attr_accessor :left, :right
 
   def initialize(number)
-    @data= number
+    @data = number
     @left = nil
     @right = nil
   end
 
   def insert(next_num)
-    if data >= next_num 
+    if data >= next_num
       !left ? @left = Bst.new(next_num) : @left.insert(next_num)
     else
       !right ? @right = Bst.new(next_num) : @right.insert(next_num)
@@ -19,22 +19,18 @@ class Bst
 
   def record_branch
     all_branches = [@data]
-    if @left
-      all_branches.unshift(@left.record_branch)
-    end
-    if @right
-      all_branches.push(@right.record_branch)
-    end
+    all_branches.unshift(@left.record_branch) if @left
+    all_branches.push(@right.record_branch) if @right
     all_branches
   end
 
   def each
     all_data = record_branch.flatten
-    return all_data.to_enum if !block_given?
-    all_data.each { |data| yield(data)}
+    return all_data.to_enum unless block_given?
+    all_data.each { |data| yield(data) }
   end
 
-  def > (other)
-    self.data > other
+  def >(other)
+    data > other
   end
 end
