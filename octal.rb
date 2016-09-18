68db5b61a235714e3ed5e6793d0307407bea71df
Rachel Minto <rachel.j.minto@gmail.com>
2016-09-18 13:48:23 -0400

    Added octal exercise.

---
+++ octal.rb
@@ -0,0 +1,19 @@
+class Octal
+  attr_accessor :octal_number
+
+  def initialize(octal_number)
+    @octal_number = octal_number
+  end
+
+  def to_decimal
+    return 0 if @octal_number.match(/[^0-7]/)
+    exponent = -1
+
+    decimal_num = @octal_number.split('').reverse.map do |digit|
+      exponent += 1
+      digit.to_i * 8 ** exponent
+    end
+
+    decimal_num.reverse.reduce(:+)
+  end
+end
--- sieve.rb
+++ sieve.rb
@@ -1,18 +1,37 @@
 class Sieve
-  attr_accessor :numbers_to_check, :primes
+  attr_accessor :primes 
+  attr_reader :limit
 
   def initialize(limit)
-    @numbers_to_check = (2..limit).to_a
     @primes = (2..limit).to_a
-  end
-
-  def reject_nonprime(prime)
-    @primes.reject! { |num| num != prime && num % prime == 0 }
+    @limit = limit
   end
 
   def primes
-    @numbers_to_check.each {|prime| reject_nonprime(prime)}
+    (2..@limit).each do |prime| 
+      @primes.reject! do |num| 
+        num != prime && num % prime == 0 
+      end 
+    end
+
     @primes
   end
 end
 
+# class Sieve
+#   attr_accessor :numbers_to_check, :primes
+
+#   def initialize(limit)
+#     @numbers_to_check = (2..limit).to_a
+#     @primes = (2..limit).to_a
+#   end
+
+#   def reject_nonprime(prime)
+#     @primes.reject! { |num| num != prime && num % prime == 0 }
+#   end
+
+#   def primes
+#     @numbers_to_check.each {|prime| reject_nonprime(prime)}
+#     @primes
+#   end
+# end
